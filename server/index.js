818bb49e62af4d2e5cda9d198010c1826f988243
jimuskin <jimuskin1234567890@gmail.com>
2020-12-12 22:27:05 +1100

    Added docker support.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("running.");
+const app = require("express")();
+
+app.get("/", (req, res) => {
+	res.json({ message: "It works." });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () =>
+	console.log(`App is listening on port ${port}`)
+);
