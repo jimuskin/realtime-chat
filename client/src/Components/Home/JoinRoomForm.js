996953a8733baeef55765a407a55936516f73f24
jimuskin <jimuskin1234567890@gmail.com>
2020-12-17 22:21:32 +1100

    Added form inputs

--- client/src/Components/Home/JoinRoomForm.js
+++ client/src/Components/Home/JoinRoomForm.js
@@ -1,32 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { TextField, Button } from "@material-ui/core";
 
 const JoinRoomForm = () => {
+	const [roomDetails, setRoomDetails] = useState({
+		username: "",
+		roomID: "",
+	});
+
+	const handleFormSubmit = (event) => {
+		event.preventDefault();
+		setRoomDetails({
+			username: "",
+			roomID: "",
+		});
+	};
+
 	return (
-		<form
-			style={{
-				paddingTop: 10,
-				paddingBottom: 20,
-				marginLeft: 50,
-				marginRight: 50,
-			}}
-		>
-			<h1>Join Room</h1>
-			<TextField label="Username" /> <br />
-			<TextField label="Room ID" /> <br />
-			<Button
-				variant="contained"
-				color="primary"
-				label="Submit"
-				type="submit"
+		<>
+			<form
 				style={{
-					marginTop: 15,
+					paddingTop: 10,
+					paddingBottom: 20,
+					marginLeft: 50,
+					marginRight: 50,
 				}}
+				onSubmit={handleFormSubmit}
 			>
-				Submit
-			</Button>
-		</form>
+				<h1>Join Room</h1>
+				<TextField
+					value={roomDetails.username}
+					label="Username"
+					onInput={(e) =>
+						setRoomDetails((prevDetails) => {
+							return {
+								...prevDetails,
+								username: e.target.value,
+							};
+						})
+					}
+				/>
+				<br />
+				<TextField
+					value={roomDetails.roomID}
+					onInput={(e) =>
+						setRoomDetails((prevDetails) => {
+							return {
+								...prevDetails,
+								roomID: e.target.value,
+							};
+						})
+					}
+					label="Room ID"
+				/>
+				<br />
+				<Button
+					variant="contained"
+					color="primary"
+					label="Submit"
+					type="submit"
+					style={{
+						marginTop: 15,
+					}}
+				>
+					Submit
+				</Button>
+			</form>
+			<h1>Form Details:</h1>
+			<p>{JSON.stringify(roomDetails)}</p>
+		</>
 	);
 };
 
