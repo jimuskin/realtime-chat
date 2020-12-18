af79d07be0ac61a8e50a032ad72375a39edd6eee
jimuskin <jimuskin1234567890@gmail.com>
2020-12-18 21:16:49 +1100

    Started progress on chat room layout

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,7 +1,23 @@
 import React from "react";
 
+import { Grid, Paper } from "@material-ui/core";
+
 const Chat = () => {
-	return <h1>Chat Room</h1>;
+	return (
+		<div style={{ padding: 15 }}>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={10}>
+					<Paper
+						noWrap
+						style={{ height: "80vh" }}
+					></Paper>
+				</Grid>
+				<Grid item xs={12} sm={2}>
+					<Paper>xs=12 sm=6</Paper>
+				</Grid>
+			</Grid>
+		</div>
+	);
 };
 
 export default Chat;
--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,20 +1,15 @@
 import React from "react";
-import {
-	Box,
-	Typography,
-	Button,
-	IconButton,
-} from "@material-ui/core";
+import { Box } from "@material-ui/core";
 
 const Navbar = () => {
 	return (
 		<Box
+			className="navigation"
 			display="flex"
 			p={1}
 			justifyContent="center"
 			alignItems="center"
 			bgcolor="primary.main"
-			border={1}
 		>
 			<h1
 				style={{
