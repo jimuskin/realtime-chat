acefc34e72df841b48c020b0fbb6b010feb4b18c
jimuskin <jimuskin1234567890@gmail.com>
2020-12-21 01:16:54 +1100

    Replaced mongo dependancies with redis

--- server/index.js
+++ server/index.js
@@ -1,5 +1,23 @@
 const app = require("express")();
 
+const redis = require("redis");
+const client = redis.createClient(6000, "192.168.205.145");
+
+client.on("connect", () => {
+	console.log("Connected to redis server.");
+	client.get("login", (err, reply) => {
+		const loginAttempts = reply ? reply : 0;
+
+		console.log(`Connected ${loginAttempts} times.`);
+
+		client.set("login", parseInt(loginAttempts) + 1);
+	});
+});
+
+client.on("error", () => {
+	console.log("An error occurred");
+});
+
 app.get("/", (req, res) => {
 	res.json({ message: "its working!" });
 });
