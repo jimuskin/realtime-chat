538d60b5e13e14dea32d03d603d2e0367892a0b9
jimuskin <jimuskin1234567890@gmail.com>
2020-12-28 12:17:17 +1100

    Lobby checks database before returning if exists.

--- server/controllers/getLobby.js
+++ server/controllers/getLobby.js
@@ -1,3 +1,5 @@
+const lobbySchema = require("../mongo/Schemas/lobbySchema");
+
 const getLobby = (req, res) => {
 	const id = req.params.id;
 
@@ -7,9 +9,25 @@ const getLobby = (req, res) => {
 		});
 	}
 
-	return res.status(200).json({
-		success: true,
-		id: id,
+	lobbySchema.find({ lobbyID: id }, (err, data) => {
+		if (err) {
+			res.status(err.status).json({
+				error: err,
+			});
+			return;
+		}
+
+		if (!(Array.isArray(data) && data.length)) {
+			res.status(404).json({
+				message: "Lobby ID does not exist.",
+			});
+			return;
+		}
+
+		res.status(200).json({
+			data: data,
+		});
+		return;
 	});
 };
 
