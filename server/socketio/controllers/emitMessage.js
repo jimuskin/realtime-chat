b1668357a6fe361884a502aeb1bd9cbc2134547b
jimuskin <jimuskin1234567890@gmail.com>
2021-01-08 14:53:20 +1100

    Relocated message storing to happen when message is actually being emitted.

--- server/socketio/controllers/emitMessage.js
+++ server/socketio/controllers/emitMessage.js
@@ -1,8 +1,33 @@
 const ioConnection = require("./ioConnection");
+const lobbySchema = require("../../mongo/Schemas/lobbySchema");
 
 const emitMessage = (lobby, messageDetails) => {
 	let io = ioConnection.io();
-	io.to(lobby).emit("server_message", messageDetails);
+
+	lobbySchema.findOneAndUpdate(
+		{
+			lobbyID: lobby,
+		},
+		{
+			$push: {
+				"messages.0": {
+					user: messageDetails.name,
+					message: messageDetails.message,
+				},
+			},
+		},
+		(err, lobbyData) => {
+			if (err) {
+				console.log(`Error ${err.status}: ${err}`);
+				return;
+			}
+
+			io.to(lobby).emit(
+				"server_message",
+				messageDetails
+			);
+		}
+	);
 };
 
 module.exports = emitMessage;
--- server/socketio/events/recieveClientMessageEvent.js
+++ server/socketio/events/recieveClientMessageEvent.js
@@ -1,33 +1,12 @@
 const emitMessage = require("../controllers/emitMessage");
-const lobbySchema = require("../../mongo/Schemas/lobbySchema");
 
 const recieveClientMessageEvent = (user, message) => {
 	const { username, lobbyID } = user;
 
-	lobbySchema.findOneAndUpdate(
-		{
-			lobbyID: lobbyID,
-		},
-		{
-			$push: {
-				"messages.0": {
-					user: username,
-					message: message,
-				},
-			},
-		},
-		(err, lobbyData) => {
-			if (err) {
-				console.log(`Error ${err.status}: ${err}`);
-				return;
-			}
-
-			emitMessage(lobbyID, {
-				name: username,
-				message: message,
-			});
-		}
-	);
+	emitMessage(lobbyID, {
+		name: username,
+		message: message,
+	});
 };
 
 module.exports = recieveClientMessageEvent;
