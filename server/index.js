06ef9f58f331106688024ed7f25b2ef819015507
jimuskin <jimuskin1234567890@gmail.com>
2021-01-16 16:22:41 +1100

    Removed redis from container (Unable to properly utilise it).

--- server/index.js
+++ server/index.js
@@ -2,15 +2,11 @@ const app = require("express")();
 
 const setupExpress = require("./setup/setupExpress");
 const setupMongo = require("./setup/setupMongo");
-const setupRedis = require("./setup/setupRedis");
 const setupSocketIO = require("./setup/setupSocketIO");
 
 const setupServer = async () => {
 	//Set up mongoDB connection.
 	let mongoSuccess = await setupMongo();
-
-	//Set up Redis connection.
-	let redisSuccess = await setupRedis();
 
 	let server = await setupExpress();
 
